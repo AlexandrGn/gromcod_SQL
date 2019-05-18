e2ecd6da3557bae1bff5f574d6f7803e9f0db321
AlexandrGn <alexandro.gn@yandex.ua>
2019-05-18 23:52:15 +0300

    -

--- src/lesson6/Demo.java
+++ src/lesson6/Demo.java
@@ -4,5 +4,9 @@ public class Demo {
     public static void main(String[] args) {
         Author author=new Author(105,"Test");
         Article article=new Article(1,"test header", "test text",author);
+        Author author1=new Author(105,"Test");
+        Article article1=new Article(1,"test header", "test text",author);
+        Author author2=new Author(105,"Test");
+        Article article2=new Article(1,"test header", "test text",author);
     }
 }

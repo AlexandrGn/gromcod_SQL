2d0a4a6753f32eba54238d5896f2efd6a017514f
AlexandrGn <alexandro.gn@yandex.ua>
2019-05-22 01:45:06 +0300

    -

--- src/lesson6/Demo.java
+++ src/lesson6/Demo.java
@@ -1,5 +1,8 @@
 package lesson6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Demo {
     public static void main(String[] args) {
         Author author=new Author(105,"Test");
@@ -8,5 +11,15 @@ public class Demo {
         Article article1=new Article(1,"test header", "test text",author);
         Author author2=new Author(105,"Test");
         Article article2=new Article(1,"test header", "test text",author);
+
+
+
+        int[] d={5,3,6};
+        System.out.println(d.length);
+        List arrayList = new ArrayList();
+        arrayList.add("sd");
+        arrayList.add(author);
+        System.out.println(arrayList);
+        List<Integer> list = new ArrayList<Integer>();
     }
 }
